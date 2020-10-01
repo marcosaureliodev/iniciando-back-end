12d949b729ac08e98a4a71a4cbee88448f2a412b
Marcos Aurelio <marcos.aureliodev@gmail.com>
2020-09-30 23:57:34 -0300

    🤓👍 criando tabela para db migrations

---
+++ src/database/migrations/1601346123723-CreateAppointments.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointments1601346123723
+    implements MigrationInterface {
+    // No method up é o que a migration irá fazer
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'appointments',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                    },
+                    {
+                        name: 'provider',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'date',
+                        type: 'timestamp with time zone',
+                        isNullable: false,
+                    },
+                ],
+            }),
+        );
+    }
+
+    // No method down vai disfazera (apagar) o que mothod up faz na migrattion
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('appointments');
+    }
+}
